751444dbb52deadd6093c89be4849f727956fed2
Adrian W <wasilewski.adrian.64@gmail.com>
2020-01-23 19:58:23 +0100

    initial commit with webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 9000,
+        watchContentBase: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader" 
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /nodemodules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.(html)$/,
+                use: ["html-loader"]
+            }
+        ]
+    }
+}
